4aab2ff156163d69b69282b7db2b0df763eabf84
Allen Chen <alchen@drwholdings.com>
2022-10-16 10:56:51 -0500

    update install & lg

--- install.sh
+++ install.sh
@@ -23,5 +23,9 @@ unzip DroidSansMono.zip -d ~/.fonts
 fc-cache -fv
 echo "done!"
 
+# zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+
 ./update_config.sh
 
