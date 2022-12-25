0d754b3a73385d3b150142e4d9d0866f24b59bee
YilunAllenChen <allenchenyilun1999@gmail.com>
2022-12-24 18:40:06 -0600

    compatibility for mac

--- update_config.sh
+++ update_config.sh
@@ -1,6 +1,14 @@
 cp -R ~/dotfiles/config/. ~/.config
-cp ~/dotfiles/.bashcustomize ~/
+cp ~/dotfiles/.shcustomize ~/
 
-LINE='source ~/.bashcustomize'
-FILE='/home/alchen/.bashrc'
+LINE='source ~/.shcustomize'
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  FILE='/home/alchen/.bashrc'
+  FZFLINE = "[ -f ~/.fzf.bash ] && source ~/.fzf.bash"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  FILE='/home/alchen/.zshrc'
+  FZFLINE = "[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh"
+fi
+
 grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+grep -qF -- "$FZFLINE" "$FILE" || echo "$FZFLINE" >> "$FILE"
