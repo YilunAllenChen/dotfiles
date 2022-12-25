a7804f19d86a2b534fc400cb70ff7616b38afff5
YilunAllenChen <allenchenyilun1999@gmail.com>
2022-12-24 18:42:15 -0600

    update

--- update_config.sh
+++ update_config.sh
@@ -3,11 +3,11 @@ cp ~/dotfiles/.shcustomize ~/
 
 LINE='source ~/.shcustomize'
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  FILE='/home/alchen/.bashrc'
-  FZFLINE = "[ -f ~/.fzf.bash ] && source ~/.fzf.bash"
+  FILE="$HOME/.bashrc"
+  FZFLINE="[ -f ~/.fzf.bash ] && source ~/.fzf.bash"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-  FILE='/home/alchen/.zshrc'
-  FZFLINE = "[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh"
+  FILE="$HOME/.zshrc"
+  FZFLINE="[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh"
 fi
 
 grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
