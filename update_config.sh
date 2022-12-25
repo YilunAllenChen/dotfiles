78a9e6f78193a15e3d44c8fae933d012995a629b
YilunAllenChen <allenchenyilun1999@gmail.com>
2022-12-24 19:42:42 -0600

    better aliases

--- update_config.sh
+++ update_config.sh
@@ -4,11 +4,8 @@ cp ~/dotfiles/.shcustomize ~/
 LINE='source ~/.shcustomize'
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   FILE="$HOME/.bashrc"
-  FZFLINE="[ -f ~/.fzf.bash ] && source ~/.fzf.bash"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   FILE="$HOME/.zshrc"
-  FZFLINE="[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh"
 fi
 
 grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
-grep -qF -- "$FZFLINE" "$FILE" || echo "$FZFLINE" >> "$FILE"
